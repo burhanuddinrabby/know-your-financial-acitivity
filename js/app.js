ea44d21b16072c16ef85bfe14b96af45fb97f9e8
Burhan Uddin <bunnub5683@gmail.com>
2022-02-16 23:26:40 +0600

    js updated

---
+++ js/app.js
@@ -0,0 +1,23 @@
+const income = document.getElementById('income');
+        const food = document.getElementById('food');
+        const rent = document.getElementById('rent');
+        const clothes = document.getElementById('clothes');
+        function calculation(){
+            const incomeAmmount = parseFloat(income.value);
+            const totalExpenses = parseFloat(food.value) + parseFloat(rent.value) + parseFloat(clothes.value);
+            const balance = incomeAmmount - totalExpenses;
+            document.getElementById('expenseTotal').innerText = totalExpenses;
+            document.getElementById('balanceTotal').innerText = balance;
+        }
+        document.getElementById('calc-btn').addEventListener('click', function(){
+            calculation();
+        });
+        const savingPercentage = document.getElementById('savingPercentage');
+        document.getElementById('save-btn').addEventListener('click', function() {
+            calculation();
+            const balance = parseFloat(income.value) - (parseFloat(food.value) + parseFloat(rent.value) + parseFloat(clothes.value));
+            const savingPercentageValue = parseFloat(savingPercentage.value);
+            const savingAmmount = parseFloat(income.value) * (savingPercentageValue / 100);
+            document.getElementById('saving-ammount').innerText = savingAmmount;
+            document.getElementById('remaing-balance').innerText = balance - savingAmmount;
+        });
