bb80918190aa183f091b6998dbe6e9ce1ff05f9f
Burhan Uddin <bunnub5683@gmail.com>
2022-02-17 12:16:43 +0600

    final form

--- js/app.js
+++ js/app.js
@@ -7,7 +7,9 @@ const inputs = document.getElementsByClassName('inp');
 function calculation(){
     if(income.value == '' || food.value == '' || rent.value == '' || clothes.value == ''/*  || savingPercentage.value == '' */){
         document.getElementById('error-msg').innerText = 'Input type cannot be empty';
-    }else if(income.value < 0 || food.value < 0 || rent.value < 0 || clothes.value < 0 || savingPercentage.value < 0){
+    }else if(income.value < 0){
+        document.getElementById('error-msg').innerText = 'Income cannot be negative';
+    }else if(food.value < 0 || rent.value < 0 || clothes.value < 0 /* || savingPercentage.value < 0 */){
         document.getElementById('error-msg').innerText = 'Cost cannot be negative';
     }else{
         const incomeAmmount = parseFloat(income.value);
@@ -15,10 +17,12 @@ function calculation(){
         const balance = incomeAmmount - totalExpenses;
         if (totalExpenses > incomeAmmount){
             document.getElementById('expenseTotal').innerText = totalExpenses;
+            document.getElementById('expenseTotal').style.color = 'red';
             document.getElementById('error-msg').innerText = 'You\'ve gone over your income';
             document.getElementById('balanceTotal').innerText = '';
         }else{
             document.getElementById('expenseTotal').innerText = totalExpenses;
+            document.getElementById('expenseTotal').style.color = 'black';
             document.getElementById('error-msg').innerText = '';
             document.getElementById('balanceTotal').innerText = balance;
             return balance;
@@ -38,10 +42,14 @@ document.getElementById('save-btn').addEventListener('click', function() {
             document.getElementById('saving-ammount').innerText = Math.round(savingAmmount);
             document.getElementById('saving-ammount').style.color = 'red';
             document.getElementById('remaing-balance').innerText = '000';
-        }else{
+        }else if(savingPercentage.value < 0){
+            document.getElementById('error-msg-saving').innerText = 'You cannot be save negative percentage of ammount';
+        }
+        else{
             document.getElementById('error-msg-saving').innerText = '';
-            document.getElementById('saving-ammount').innerText = Math.round(savingAmmount);
-            document.getElementById('remaing-balance').innerText = Math.round(balance - savingAmmount);
+            document.getElementById('saving-ammount').innerText = Math.ceil(savingAmmount);
+            document.getElementById('saving-ammount').style.color = 'black';
+            document.getElementById('remaing-balance').innerText = Math.floor(balance - savingAmmount);
         }
     }
 });
