350b4aabf8b9fa3e1b8e654dc286997d2a036417
Burhan Uddin <bunnub5683@gmail.com>
2022-02-17 12:31:39 +0600

    comment added to understand

--- js/app.js
+++ js/app.js
@@ -1,15 +1,23 @@
+//getting input values
 const income = document.getElementById('income');
 const food = document.getElementById('food');
 const rent = document.getElementById('rent');
 const clothes = document.getElementById('clothes');
 const savingPercentage = document.getElementById('savingPercentage');
 const inputs = document.getElementsByClassName('inp');
+
+//calculation cost 
 function calculation(){
+    //if user leaves an empty input
     if(income.value == '' || food.value == '' || rent.value == '' || clothes.value == ''/*  || savingPercentage.value == '' */){
         document.getElementById('error-msg').innerText = 'Input type cannot be empty';
-    }else if(income.value < 0){
+    }
+    //when income is not negative 
+    else if(income.value < 0){
         document.getElementById('error-msg').innerText = 'Income cannot be negative';
-    }else if(food.value < 0 || rent.value < 0 || clothes.value < 0 /* || savingPercentage.value < 0 */){
+    }
+    //cost cannot be negative
+    else if(food.value < 0 || rent.value < 0 || clothes.value < 0 /* || savingPercentage.value < 0 */){
         document.getElementById('error-msg').innerText = 'Cost cannot be negative';
     }else{
         const incomeAmmount = parseFloat(income.value);
@@ -29,14 +37,18 @@ function calculation(){
         }
     }
 }
+//calculate the balance and expenses
 document.getElementById('calc-btn').addEventListener('click', function(){
     calculation();
 });
+//savings percentage and calculate the balance
 document.getElementById('save-btn').addEventListener('click', function() {
     const balance = calculation();
+    //if user leaves an empty input
     if (income.value != '' && income.value > 0 && food.value >= 0 && rent.value >= 0 && clothes.value >= 0 && savingPercentage.value >= 0 && balance >= 0) {
         const savingPercentageValue = parseFloat(savingPercentage.value);
         const savingAmmount = parseFloat(income.value) * (savingPercentageValue / 100);
+        //when saving ammount is greater than balance
         if (savingAmmount > balance) {
             document.getElementById('error-msg-saving').innerText = 'You can\'t save more than you have';
             document.getElementById('saving-ammount').innerText = Math.round(savingAmmount);
